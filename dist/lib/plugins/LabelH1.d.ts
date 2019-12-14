a89cb81c0aa0dffefd4448a4bdbb120103d5a222
Kevin Elliott <kevin@welikeinc.com>
2019-12-13 16:20:09 -0800

    Remove dist from repo

--- dist/lib/plugins/LabelH1.d.ts
+++
@@ -1,9 +0,0 @@
-import { DecoderPlugin } from '../DecoderPlugin';
-export declare class Label5Z extends DecoderPlugin {
-    qualifiers(): {
-        labels: string[];
-    };
-    decode(message: any): any;
-}
-declare const _default: {};
-export default _default;
