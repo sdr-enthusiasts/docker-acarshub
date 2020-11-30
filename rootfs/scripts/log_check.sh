3e90bda1801b561e57989820ce3d066b0f292fed
fredclausen <clausen.fred@icloud.com>
2020-11-30 06:36:32 -0700

    s6 fatal fixed, more script changes

--- rootfs/scripts/log_check.sh
+++ rootfs/scripts/log_check.sh
@@ -7,12 +7,15 @@
 # But I figure removing the first 4999 lines is good enough.
 # I don't know why
 
+MAX_LINES=1000
+TRIM_TO=500
+
 if [ -f "/run/acars/acars.json" ]; then
 	total_lines=$(wc -l /run/acars/acars.json | awk '/[0-9]+/{print $1}')
 
-	if (( $total_lines > 10000 )); then
+	if (( $total_lines > $MAX_LINES )); then
 		echo "Trimming acars.json"
-		let index=total_lines-5000
+		let index=total_lines-TRIM_TO
 	    sed -i "${index},${total_lines}!d" /run/acars/acars.json
 	fi
 fi
@@ -20,9 +23,9 @@ fi
 if [ -f "/run/acars/vdlm.json" ]; then
 	total_lines_vdlm=$(wc -l /run/acars/vdlm.json | awk '/[0-9]+/{print $1}')
 
-	if (( $total_lines_vdlm > 10000 )); then
+	if (( $total_lines_vdlm > $MAX_LINES )); then
 	   echo "Trimming vdlm.json"
-	   let index=total_lines_vdlm-5000
+	   let index=total_lines_vdlm-TRIM_TO
  	   sed -i "${index},${total_lines_vdlm}!d" /run/acars/vdlm.json
 	fi
 fi
