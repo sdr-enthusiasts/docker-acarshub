0e2161c8a1bdede6c88e6b882ea73387e44ca90c
Mike Nye <mike.nye@gmail.com>
2020-12-06 11:34:55 +0800

    Fix linting for shellcheck

--- rootfs/scripts/log_check.sh
+++ rootfs/scripts/log_check.sh
@@ -10,22 +10,22 @@
 MAX_LINES=1000
 TRIM_TO=500
 
-if [ -f "/run/acars/acars.json" ]; then
+if [[ -f "/run/acars/acars.json" ]]; then
 	total_lines=$(wc -l /run/acars/acars.json | awk '/[0-9]+/{print $1}')
 
-	if (( $total_lines > $MAX_LINES )); then
+	if (( total_lines > MAX_LINES )); then
 		echo "Trimming acars.json"
-		let index=total_lines-TRIM_TO
+		index=$((total_lines - TRIM_TO))
 	    sed -i "${index},${total_lines}!d" /run/acars/acars.json
 	fi
 fi
 
-if [ -f "/run/acars/vdlm.json" ]; then
+if [[ -f "/run/acars/vdlm.json" ]]; then
 	total_lines_vdlm=$(wc -l /run/acars/vdlm.json | awk '/[0-9]+/{print $1}')
 
-	if (( $total_lines_vdlm > $MAX_LINES )); then
+	if (( total_lines_vdlm > MAX_LINES )); then
 	   echo "Trimming vdlm.json"
-	   let index=total_lines_vdlm-TRIM_TO
+	   index=$((total_lines_vdlm - TRIM_TO))
  	   sed -i "${index},${total_lines_vdlm}!d" /run/acars/vdlm.json
 	fi
 fi
