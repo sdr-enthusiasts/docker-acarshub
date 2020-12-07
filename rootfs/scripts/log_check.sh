82dc018021e109d58e96e6bcc59f968487a77bc3
fredclausen <clausen.fred@icloud.com>
2020-12-06 18:17:51 -0700

    Update log_check.sh

--- rootfs/scripts/log_check.sh
+++ rootfs/scripts/log_check.sh
@@ -2,49 +2,51 @@
 #shellcheck shell=bash
 
 # Script to trim the log files down
-# Arbitrarily picked 1000 lines as the upper limit.
 
+# Number of lines that will trigger a cull
 MAX_LINES=1000
-ACARS_PATH="/run/acars/acars.json" &>/dev/null
-VDLM_PATH="/run/acars/vdlm.json" &>/dev/null
+# Number of lines that the log should have at the end
+TARGET_LINES=500
+ACARS_PATH="/run/acars/acars.json"
+VDLM_PATH="/run/acars/vdlm.json"
 
 # create semaphore file to keep the process from starting
 touch /run/acars/stoprun.job
 
-# kill the decoders
-
-
-
-if pgrep -x 'acarsdec' > /dev/null
-then
-	echo "Stopping ACARS"
-	ps aux | pgrep -x 'acarsdec' | xargs kill
-fi
-
-if pgrep -x 'vdlm2dec' > /dev/null
-then
-	echo "Stopping VDLM"
-	ps aux | pgrep -x 'vdlm2dec' | xargs kill 
-fi
-
 if [[ -f "/run/acars/acars.json" ]]; then
-	total_lines_acars=$(wc -l < $ACARS_PATH)
+    total_lines_acars=$(wc -l < $ACARS_PATH)
 
-	if (( total_lines_acars > MAX_LINES )); then
-		echo "Trimming acars.json"
-		num_lines_to_trim_acars=$((total_lines_acars - MAX_LINES))
-	    sed -i "1,${num_lines_to_trim_acars}d" $ACARS_PATH
-	fi
+    if (( total_lines_acars > MAX_LINES )); then
+        # Log file too large
+        # Kill acarsdec if started
+        if pgrep -x 'acarsdec' > /dev/null
+        then
+            echo "[logcheck] Stopping ACARS"
+            ps aux | pgrep -x 'acarsdec' | xargs kill
+        fi
+
+        echo "[logcheck] Trimming acars.json"
+        num_lines_to_trim_acars=$((total_lines_acars - TARGET_LINES))
+        sed -i "1,${num_lines_to_trim_acars}d" $ACARS_PATH
+    fi
 fi
 
 if [[ -f $VDLM_PATH ]]; then
-	total_lines_vdlm=$(wc -l < $VDLM_PATH)
+    total_lines_vdlm=$(wc -l < $VDLM_PATH)
 
-	if (( total_lines_vdlm > MAX_LINES )); then
-	   echo "Trimming vdlm.json"
-	   num_lines_to_trim_vdlm=$((total_lines_vdlm - MAX_LINES))
- 	   sed -i "1,${num_lines_to_trim_vdlm}d" $VDLM_PATH
-	fi
+    if (( total_lines_vdlm > MAX_LINES )); then
+        # Log file too large
+        # Kill vdlmdec if started
+        if pgrep -x 'vdlm2dec' > /dev/null
+        then
+            echo "[logcheck] Stopping VDLM"
+            ps aux | pgrep -x 'vdlm2dec' | xargs kill 
+        fi
+
+        echo "[logcheck] Trimming vdlm.json"
+        num_lines_to_trim_vdlm=$((total_lines_vdlm - TARGET_LINES))
+        sed -i "1,${num_lines_to_trim_vdlm}d" $VDLM_PATH
+    fi
 fi
 
 # remove the semaphore
