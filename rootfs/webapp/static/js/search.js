54d058a3a35aed864d63c985856ace62c62eb464
Fred <43556888+fredclausen@users.noreply.github.com>
2021-01-07 15:32:39 -0700

    Search page CSS/JS updates

--- rootfs/webapp/static/js/search.js
+++ rootfs/webapp/static/js/search.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
     socket.on('newmsg', function(msg) {
         //console.log("Received msg" + msg.msghtml);
         console.log("Received msg");
-        console.log(msg);
         //maintain a list of 1 msgs
         if (msgs_received.length >= 1){
             msgs_received.shift();
@@ -25,16 +24,19 @@ $(document).ready(function(){
         }
         // Lets check and see if the results match the current search string
         var display = '';
+        var display_nav_results = '';
         if(msg.search_term == current_search) {            
             msgs_received.push(msg.msghtml);
             num_results.push(msg.num_results);
             for (var i = 0; i < msgs_received.length; i++){
                 display = display_messages(msgs_received[i], true);
-                display = display_search(display, current_page, num_results[i]);
+                display_nav_results = display_search(current_page, num_results[i]);
                 //msgs_string = '<p>' + msgs_received[i].toString() + '</p>' + msgs_string;
             }
         }
         $('#log').html(display);
+        console.log(display_nav_results);
+        $('#num_results').html(display_nav_results);
         window.scrollTo(0, 0);
     });
 
@@ -42,11 +44,12 @@ $(document).ready(function(){
         var old_search = current_search;
         current_search = document.getElementById("search_term").value;
         var field = document.getElementById("dbfield").value;
-        current_page = 0;
-        if(current_search != '' && current_search != old_search)
+        if(current_search != '' && current_search != old_search) {
+            current_page = 0;
             socket.emit('query', {'search_term': current_search, 'field': field}, namespace='/search');
-        else if(current_search == '')
+        } else if(current_search == '') {
             $('#log').html('');
+        }
     });
 
     //noop
@@ -73,18 +76,20 @@ function jumppage() {
 }
 
 
-function display_search(html, current, total) {
+function display_search(current, total) {
+    html = '';
     total_pages = 0;
 
     if(total == 0)
-        return html + "<p>No results</p>";
+        return html + "No results";
 
     if(total % 20 != 0)
         total_pages = ~~(total / 20) + 1;
     else
         total_pages = ~~(total / 20);
 
-    html += `<p>Found ${total} results in ${total_pages} pages.</p><p>`
+    html += '<table class="search"><thead><th class="search_label"></th><th class="search_term"></th></thead>';
+    html += `<tr><td colspan="2">Found ${total} results in ${total_pages} pages.</td></tr>`;
 
     // Determine -/+ range. We want to show -/+ 5 pages from current index
 
@@ -95,34 +100,41 @@ function display_search(html, current, total) {
         low_end = current - 5;
 
     if(high_end > total_pages)
-        high_end = total_pages
+        high_end = total_pages;
 
-    if(low_end > 0) {
-        if(low_end > 5)
-            html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${low_end - 5})\"><<</a>`
-        else
-            html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(0)\"><<</a>`
-    }
+    if(total_pages != 1) {
+        html += "<tr><td colspan=\"2\">";
 
-    for(var i = low_end; i < high_end; i++) {
-        if(i == current) {
-            html += `${i+1} `;
+        if(low_end > 0) {
+            if(low_end > 5)
+                html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${low_end - 5})\"><< </a>`;
+            else
+                html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(0)\"><< </a>`;
         }
-        else {
-            html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${i})\">${i+1}</a> `;
-        }
-    }
 
-    if(high_end != total_pages) {
-        if(high_end + 5 < total_pages)
-            html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${high_end + 4})\">>></a>`;
-        else
-            html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${high_end}\")>>></a>`;
+        for(var i = low_end; i < high_end; i++) {
+            if(i == current) {
+                html += ` ${i+1} `;
+            }
+            else {
+                html += ` <a href=\"#\" id=\"search_page\" onclick=\"runclick(${i})\">${i+1}</a> `;
+            }
+        }
+
+        if(high_end != total_pages) {
+            if(high_end + 5 < total_pages)
+                html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${high_end + 4})\" >>></a>`;
+            else
+                html += `<a href=\"#\" id=\"search_page\" onclick=\"runclick(${high_end}\")> >></a>`;
+        }
     }
 
     if(total_pages > 5) {
-        html += "<p><form><label>Jump to page: </label> <input type=\"text\" id=\"jump\"><p></form>";
-        html += "<a href=\"javascript:void(0);\" id=\"jump_page\" onclick=\"jumppage()\">Submit</a>";
+        html += "</td></tr><tr><td class=\"search_label\"><label>Jump to page:</label></td><td class=\"search_term\"><input type=\"text\" id=\"jump\"><p></td></tr>";
+        html += "<tr><td class=\"search_label\"></td><td class=search_term><a href=\"#\" onclick=\"jumppage()\">Run Search</a></td></tr></table>"
+        html += "<div id=\"error_message\"></div>";
+    } else {
+        html += "</td></tr></table>";
         html += "<div id=\"error_message\"></div>";
     }
 
