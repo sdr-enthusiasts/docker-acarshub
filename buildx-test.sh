6cc9b3dd271040034779050f8d4578aa65ab5f60
Mike Nye <mike.nye@gmail.com>
2021-01-18 11:03:39 +0800

    Update local build scripts

--- buildx-test.sh
+++ buildx-test.sh
@@ -11,6 +11,15 @@ docker context use default
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use cluster
 
-# Build & push latest
+# Generate local dockerfile
 ./generate_local_dockerfile.sh
+
+# Build airframesio/acars-decoder-typescript
+# Copy /src/acars-decoder-typescript.tgz out of image
+docker build --file ./Dockerfile.acars-decoder-typescript -t acars-decoder-typescript:latest .
+id=$(docker create acars-decoder-typescript:latest)
+docker cp $id:/src/acars-decoder-typescript.tgz - > ./acars-decoder-typescript.tgz
+docker rm -v $id
+
+# Build & push latest
 docker buildx build -f Dockerfile.local -t "${REPO}/${IMAGE}:test" --compress --push --platform "${PLATFORMS}" .
