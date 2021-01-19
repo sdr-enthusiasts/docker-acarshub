f19ba081844f9989e021ae55a0c26cf65daf69c6
Fred <43556888+fredclausen@users.noreply.github.com>
2021-01-19 08:23:45 -0700

    RRD color tweak

--- rootfs/webapp/acarshub_rrd.py
+++ rootfs/webapp/acarshub_rrd.py
@@ -51,19 +51,19 @@ def update_graphs():
 
     if os.getenv("ENABLE_ACARS", default=False):
         args_all.append("DEF:messages-acars=/run/acars/acarshub.rrd:ACARS:AVERAGE")
-        args_all.append("LINE1:messages-acars#660A60:ACARS")
+        args_all.append("LINE1:messages-acars#770BE3:ACARS")
         args_acars.append("DEF:messages-acars=/run/acars/acarshub.rrd:ACARS:AVERAGE")
-        args_acars.append("LINE1:messages-acars#660A60:ACARS")
+        args_acars.append("LINE1:messages-acars#770BE3:ACARS")
 
     if os.getenv("ENABLE_VDLM", default=False):
         args_all.append("DEF:messages-vdlm=/run/acars/acarshub.rrd:VDLM:AVERAGE")
-        args_all.append("LINE1:messages-vdlm#1E73BE:VDLM")
+        args_all.append("LINE1:messages-vdlm#0000ff:VDLM")
         args_vdlm.append("DEF:messages-vdlm=/run/acars/acarshub.rrd:VDLM:AVERAGE")
-        args_vdlm.append("LINE1:messages-vdlm#1E73BE:VDLM")
+        args_vdlm.append("LINE1:messages-vdlm#0000ff:VDLM")
 
     if os.getenv("ENABLE_ACARS", default=False) and os.getenv("ENABLE_VDLM", default=False):
         args_all.append("DEF:messages-total=/run/acars/acarshub.rrd:TOTAL:AVERAGE")
-        args_all.append("LINE1:messages-total#C850B0:Total")
+        args_all.append("LINE1:messages-total#00ff00:Total")
 
     args_error.append("DEF:messages-error=/run/acars/acarshub.rrd:ERROR:AVERAGE")
     args_error.append("LINE1:messages-error#FF0000:Errors")
