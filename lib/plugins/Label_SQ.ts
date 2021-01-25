4288b400c0771eb44d241e56ec858be7b0fd73e0
Kevin Elliott <kevin@welikeinc.com>
2021-01-25 09:55:33 -0800

    Improvements for Label SQ decoding

--- lib/plugins/Label_SQ.ts
+++ lib/plugins/Label_SQ.ts
@@ -15,65 +15,87 @@ export class Label_SQ extends DecoderPlugin {
 
     decodeResult.raw.preamble = message.text.substring(0, 4);
     decodeResult.raw.version = message.text.substring(1, 2);
-
-    decodeResult.raw.network = 'Unknown';
-    // if (message.text && message.text !== '') {
-    //   decodeResult.raw.network = this.store.state.acarsData.labels.SQ.decoderHints.brands[message.text[3]]; // eslint-disable-line max-len
-    // }
+    decodeResult.raw.network = message.text.substring(3, 4);
 
     if (decodeResult.raw.version === '2') {
       const regex = /0(\d)X(?<org>\w)(?<iata>\w\w\w)(?<icao>\w\w\w\w)(?<station>\d)(?<lat>\d+)(?<latd>[NS])(?<lng>\d+)(?<lngd>[EW])V(?<vfreq>\d+)\/.*/;
       const result = message.text.match(regex);
 
-      if (result.length >= 8) {
-        decodeResult.raw.iataCode = result.groups.iata;
-        decodeResult.raw.icaoCode = result.groups.icao;
-        decodeResult.raw.stationNumber = result.groups.station;
-        decodeResult.raw.airport = this.decoder.lookupAirportByIata(decodeResult.raw.iataCode);
-        decodeResult.raw.ground_station_position = {
-          latitude: (Number(result.groups.lat) / 100) * (result.groups.latd === 'S' ? -1 : 1),
-          longitude: (Number(result.groups.lng) / 100) * (result.groups.lngd === 'W' ? -1 : 1)
+      if (result && result.length >= 8) {
+        decodeResult.raw.groundStation = {
+          number: result.groups.station,
+          iataCode: result.groups.iata,
+          icaoCode: result.groups.icao,
+          coordinates: {
+            latitude: (Number(result.groups.lat) / 100) * (result.groups.latd === 'S' ? -1 : 1),
+            longitude: (Number(result.groups.lng) / 100) * (result.groups.lngd === 'W' ? -1 : 1)
+          }
         }
-        decodeResult.raw.vdlFrequency = result.groups.vfreq;
+        decodeResult.raw.vdlFrequency = result.groups.vfreq / 1000.0;
       }
     }
 
     decodeResult.formatted.description = 'Ground Station Squitter';
-    decodeResult.formatted.items = {
-      network: {
+
+    var formattedNetwork = 'Unknown';
+    if (decodeResult.raw.network == 'A') {
+      formattedNetwork = 'ARINC';
+    } else if (decodeResult.raw.network == 'S') {
+      formattedNetwork = 'SITA';
+    }
+    decodeResult.formatted.items = [
+      {
+        type: 'network',
         label: 'Network',
-        value: decodeResult.raw.network,
+        value: formattedNetwork,
       },
-    };
-    if (decodeResult.raw.icaoCode && decodeResult.raw.stationNumber) {
-      decodeResult.formatted.items.groundStation = {
+      {
+        type: 'version',
+        label: 'Version',
+        value: decodeResult.raw.version,
+      }
+    ];
+    if (decodeResult.raw.groundStation.icaoCode && decodeResult.raw.groundStation.number) {
+      decodeResult.formatted.items.push({
+        type: 'ground_station',
         label: 'Ground Station',
-        value: `${decodeResult.raw.icaoCode}${decodeResult.raw.stationNumber}`,
-      };
+        value: `${decodeResult.raw.groundStation.icaoCode}${decodeResult.raw.groundStation.number}`,
+      });
     }
-    if (decodeResult.raw.iataCode) {
-      decodeResult.formatted.items.iata = {
+    if (decodeResult.raw.groundStation.iataCode) {
+      decodeResult.formatted.items.push({
+        type: 'iataCode',
         label: 'IATA',
-        value: decodeResult.raw.iataCode,
-      };
+        value: decodeResult.raw.groundStation.iataCode,
+      });
     }
-    if (decodeResult.raw.icaoCode) {
-      decodeResult.formatted.items.icao = {
+    if (decodeResult.raw.groundStation.icaoCode) {
+      decodeResult.formatted.items.push({
+        type: 'icaoCode',
         label: 'ICAO',
-        value: decodeResult.raw.icaoCode,
-      };
+        value: decodeResult.raw.groundStation.icaoCode,
+      });
     }
-    if (decodeResult.raw.latitude) {
-      decodeResult.formatted.items.coordinates = {
-        label: 'Ground Station Position',
-        value: `${decodeResult.raw.latitude}, ${decodeResult.raw.longitude}`,
-      };
+    if (decodeResult.raw.groundStation.coordinates.latitude) {
+      decodeResult.formatted.items.push({
+        type: 'coordinates',
+        label: 'Ground Station Location',
+        value: `${decodeResult.raw.groundStation.coordinates.latitude}, ${decodeResult.raw.groundStation.coordinates.longitude}`,
+      });
     }
-    if (decodeResult.raw.airport) {
-      decodeResult.formatted.items.airport = {
+    if (decodeResult.raw.groundStation.airport) {
+      decodeResult.formatted.items.push({
+        type: 'airport',
         label: 'Airport',
-        value: `${decodeResult.raw.airport.name} (${decodeResult.raw.airport.icao}) in ${decodeResult.raw.airport.location}`,
-      };
+        value: `${decodeResult.raw.groundStation.airport.name} (${decodeResult.raw.groundStation.airport.icao}) in ${decodeResult.raw.groundStation.airport.location}`,
+      });
+    }
+    if (decodeResult.raw.vdlFrequency) {
+      decodeResult.formatted.items.push({
+        type: 'vdlFrequency',
+        label: 'VDL Frequency',
+        value: `${decodeResult.raw.vdlFrequency} MHz`
+      });
     }
     decodeResult.decoded = true;
     decodeResult.decoder.decodeLevel = 'full';
