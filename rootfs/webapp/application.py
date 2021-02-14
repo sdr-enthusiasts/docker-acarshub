d5d0c22159b3663f4f2a86cabfd4d14a097fd375
Fred <43556888+fredclausen@users.noreply.github.com>
2021-02-14 12:18:30 -0700

    Make sure alerts list is not none

--- rootfs/webapp/application.py
+++ rootfs/webapp/application.py
@@ -496,7 +496,8 @@ def get_alerts(message, namespace):
 
     requester = request.sid
     results = acarshub.acarshub_db.search_alerts(icao=message['icao'], text=message['text'], flight=message['flight'], tail=message['tail'])
-    results_sorted = results.reverse()
+    if results is not None:
+        results.reverse()
 
     for item in [item for item in (results or [])]:
         socketio.emit('newmsg', {'msghtml': acarshub.update_keys(json.loads(item))}, to=requester, namespace="/alerts")
