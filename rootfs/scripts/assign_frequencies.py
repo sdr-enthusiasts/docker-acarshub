9585ec11d52c0f1307e84e257fc2aab70ab2a297
Fred <43556888+fredclausen@users.noreply.github.com>
2021-05-07 09:35:57 -0600

    handle space in serial during path creation

--- rootfs/scripts/assign_frequencies.py
+++ rootfs/scripts/assign_frequencies.py
@@ -98,7 +98,7 @@ def generate_output_files(serials, decoder, freqs_string):
             # So that healthcheck works right and system status will show an error we'll generate a fake startup file
             if splitSerial not in serial_ids:
                 print(f"ERROR: SDR {splitSerial} could not be mapped")
-                path = servicesd_path + f"{decoder}-" + splitSerial
+                path = servicesd_path + f"{decoder}-" + splitSerial.replace(" ", "-")
                 os.makedirs(path)
                 shutil.copyfile(f"../etc/template/bad/run", path + "/run")
             # raise RuntimeError("/usr/local/bin/rtl_serial_to_deviceid.sh", rtlSerialToDeviceIDstdout, rtlSerialToDeviceIDstderr)
@@ -110,7 +110,7 @@ def generate_output_files(serials, decoder, freqs_string):
             )  # Casting as an str to keep the variable type consistent with all of the input possibilities
 
         if not path:
-            path = servicesd_path + f"{decoder}-" + splitSerial
+            path = servicesd_path + f"{decoder}-" + splitSerial.replace(" ", "-")
             os.makedirs(path)
             shutil.copyfile(f"../etc/template/{decoder}/run", path + "/run")
 
