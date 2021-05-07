4421d884b8e6c7419b30d55513b97555caea0f0e
Fred <43556888+fredclausen@users.noreply.github.com>
2021-05-07 09:56:59 -0600

    Helper script for building and gitignore updates related

---
+++ build-test-no-cluster.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+#shellcheck shell=sh
+
+set -xe
+
+REPO=fredclausen
+IMAGE=acarshub
+# PLATFORMS="linux/arm64,linux/arm/v6,linux/arm/v7"
+
+# docker context use default
+# export DOCKER_CLI_EXPERIMENTAL="enabled"
+# docker buildx use cluster
+
+# Generate local dockerfile
+./generate_local_dockerfile.sh
+
+#  move the local built copy of airframes out of rootfs
+mv rootfs/webapp/static/airframes-acars-decoder .
+
+# Build airframesio/acars-decoder-typescript
+# Copy /src/acars-decoder-typescript.tgz out of image
+docker build --file ./Dockerfile.acars-decoder-typescript -t acars-decoder-typescript:latest .
+id=$(docker create acars-decoder-typescript:latest)
+docker cp "$id":/src/acars-decoder-typescript.tgz ./acars-decoder-typescript.tgz
+docker rm -v "$id"
+
+# Build & push latest
+docker build -f Dockerfile.local -t "${REPO}/${IMAGE}:test" .
+
+# Clean up
+rm ./acars-decoder-typescript.tgz
+
+#  move the local built copy of airframes out of rootfs
+mv airframes-acars-decoder rootfs/webapp/static/airframes-acars-decoder
