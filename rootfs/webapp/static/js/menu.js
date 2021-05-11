39fbfc713c8a62b07cbd140a7c0b353e6aa0d552
Fred <43556888+fredclausen@users.noreply.github.com>
2021-05-10 20:55:19 -0600

    prettier pre-commit hook

--- rootfs/webapp/static/js/menu.js
+++ rootfs/webapp/static/js/menu.js
@@ -1,46 +1,53 @@
-var acars_path = document.location.pathname.replace(/about|search|stats|status|alerts/gi, "");
-acars_path += acars_path.endsWith("/") ? "" : "/"
+var acars_path = document.location.pathname.replace(
+  /about|search|stats|status|alerts/gi,
+  ""
+);
+acars_path += acars_path.endsWith("/") ? "" : "/";
 var acars_url = document.location.origin + acars_path;
 
-function generate_menu(){
-	html = '<nav>';
-   html += '<ul class="primary">';
-   html += `<li class="img_box"><img src="${acars_url}static/images/acarshubsquare.png" class="banner_img"></li>`;
-   html += `<li><a href="${acars_url}">Live Messages</a></li>`;
-   html += `<li><a href="${acars_url}search">Search Database</a></li>`;
-   html += `<li><a href="${acars_url}alerts">Alerts&nbsp;<span id="alert_count"></span></li>`;
-   html += `<li><a href="${acars_url}stats">Statistics</a></li></ul>`;
-   html += '</nav>';
-   $('#links').html(html);
- }
+function generate_menu() {
+  html = "<nav>";
+  html += '<ul class="primary">';
+  html += `<li class="img_box"><img src="${acars_url}static/images/acarshubsquare.png" class="banner_img"></li>`;
+  html += `<li><a href="${acars_url}">Live Messages</a></li>`;
+  html += `<li><a href="${acars_url}search">Search Database</a></li>`;
+  html += `<li><a href="${acars_url}alerts">Alerts&nbsp;<span id="alert_count"></span></li>`;
+  html += `<li><a href="${acars_url}stats">Statistics</a></li></ul>`;
+  html += "</nav>";
+  $("#links").html(html);
+}
 
- function generate_stat_submenu(acars, vdlm) {
- 	var text = "";
- 	if(acars == true && vdlm == true) {
- 		text = "<p><a href=\"javascript:update_prefix('')\" id=\"pause_updates\" class=\"spread_text\">Combined Graphs</a></p>";
- 	}
+function generate_stat_submenu(acars, vdlm) {
+  var text = "";
+  if (acars == true && vdlm == true) {
+    text =
+      '<p><a href="javascript:update_prefix(\'\')" id="pause_updates" class="spread_text">Combined Graphs</a></p>';
+  }
 
- 	if(acars) {
- 		text += "<p><a href=\"javascript:update_prefix('acars')\" id=\"pause_updates\" class=\"spread_text\">ACARS Graphs</a></p>";
- 	}
+  if (acars) {
+    text +=
+      '<p><a href="javascript:update_prefix(\'acars\')" id="pause_updates" class="spread_text">ACARS Graphs</a></p>';
+  }
 
- 	if(vdlm) {
- 		text += "<p><a href=\"javascript:update_prefix('vdlm')\" id=\"pause_updates\" class=\"spread_text\">VDLM Graphs</a></p>";
- 	}
+  if (vdlm) {
+    text +=
+      '<p><a href="javascript:update_prefix(\'vdlm\')" id="pause_updates" class="spread_text">VDLM Graphs</a></p>';
+  }
 
- 	text += "<p><a href=\"javascript:update_prefix('error')\" id=\"pause_updates\" class=\"spread_text\">Message Error Graphs</a></p>";
-    //text += "<p><a href=\"javascript:grab_freqs()\" id=\"pause_updates\" class=\"spread_text\">Frequency Counts</a></p>";
-    text += "<hr>";
-    text += "<p><span id=\"freqs\"></span></p>";
-    text += "<p><span id=\"msgs\"></span></p>";
- 	$('#stat_menu').html(text);
- }
+  text +=
+    '<p><a href="javascript:update_prefix(\'error\')" id="pause_updates" class="spread_text">Message Error Graphs</a></p>';
+  //text += "<p><a href=\"javascript:grab_freqs()\" id=\"pause_updates\" class=\"spread_text\">Frequency Counts</a></p>";
+  text += "<hr>";
+  text += '<p><span id="freqs"></span></p>';
+  text += '<p><span id="msgs"></span></p>';
+  $("#stat_menu").html(text);
+}
 
- function generate_footer() {
-    var html = `<strong><a href="${acars_url}about">ACARS Hub Help/About</a> | <a href="https://github.com/fredclausen/docker-acarshub" target="_blank">Project Github</a> | \
+function generate_footer() {
+  var html = `<strong><a href="${acars_url}about">ACARS Hub Help/About</a> | <a href="https://github.com/fredclausen/docker-acarshub" target="_blank">Project Github</a> | \
      <a href="https://discord.gg/sTf9uYF"><img src="https://img.shields.io/discord/734090820684349521" alt="discord"></a> | \
      <span id="system_status"><a href="${acars_url}status">System Status: <span class="green">Okay</a></span></span></strong>\
      <span id="disconnect"></span> \
      <span class="align_right">Pre-Release</span>`;
-    $('#footer_div').html(html);
- }
+  $("#footer_div").html(html);
+}
--- rootfs/webapp/static/js/status.js
+++ rootfs/webapp/static/js/status.js
@@ -1,44 +1,60 @@
 var socket;
-var acars_path = document.location.pathname.replace(/about|search|stats|status|alerts/gi, "");
-acars_path += acars_path.endsWith("/") ? "" : "/"
+var acars_path = document.location.pathname.replace(
+  /about|search|stats|status|alerts/gi,
+  ""
+);
+acars_path += acars_path.endsWith("/") ? "" : "/";
 var acars_url = document.location.origin + acars_path;
 
-$(document).ready(function(){
-    generate_menu();
-    generate_footer();
+$(document).ready(function () {
+  generate_menu();
+  generate_footer();
 
-    socket = io.connect(`${document.location.origin}/status`, {
-      'path': acars_path + 'socket.io',
-    });
-
-    socket.on('system_status', function(msg) {
-      if(msg.status.error_state == true) {
-          $('#system_status').html(`<a href="${acars_url}status">System Status: <span class="red">Error</a></span>`);
-      } else {
-          $('#system_status').html(`<a href="${acars_url}status">System Status: <span class="green">Okay</a></span>`);
-      }
-
-      $('#log').html(decode_status(msg.status.error_state, msg.status.decoders, msg.status.servers, msg.status.feeders, msg.status.global, msg.status.stats));
+  socket = io.connect(`${document.location.origin}/status`, {
+    path: acars_path + "socket.io",
   });
 
-  socket_alerts.on('disconnect', function() {
+  socket.on("system_status", function (msg) {
+    if (msg.status.error_state == true) {
+      $("#system_status").html(
+        `<a href="${acars_url}status">System Status: <span class="red">Error</a></span>`
+      );
+    } else {
+      $("#system_status").html(
+        `<a href="${acars_url}status">System Status: <span class="green">Okay</a></span>`
+      );
+    }
+
+    $("#log").html(
+      decode_status(
+        msg.status.error_state,
+        msg.status.decoders,
+        msg.status.servers,
+        msg.status.feeders,
+        msg.status.global,
+        msg.status.stats
+      )
+    );
+  });
+
+  socket_alerts.on("disconnect", function () {
     connection_status();
   });
 
-  socket_alerts.on('connect_error', function() {
-      connection_status();
+  socket_alerts.on("connect_error", function () {
+    connection_status();
   });
 
-  socket_alerts.on('connect_timeout', function() {
-      connection_status();
+  socket_alerts.on("connect_timeout", function () {
+    connection_status();
   });
 
-  socket_alerts.on('connect', function() {
-      connection_status(true);
+  socket_alerts.on("connect", function () {
+    connection_status(true);
   });
 
-  socket_alerts.on('reconnect', function() {
-      connection_status(true);
+  socket_alerts.on("reconnect", function () {
+    connection_status(true);
   });
 });
 
@@ -51,54 +67,67 @@ function decode_status(status, decoders, servers, feeders, receivers, stats) {
   const keys_stats = Object.keys(stats);
 
   html_output += '<span class="monofont">';
-  html_output += "System:".padEnd(55,'.');
-  if(status) {
+  html_output += "System:".padEnd(55, ".");
+  if (status) {
     html_output += '<strong><span class="red">DEGRADED</span></strong>';
   } else {
     html_output += '<strong><span class="green">Ok</span></strong>';
   }
-  html_output += '<br>';
+  html_output += "<br>";
 
   keys_decoder.forEach((key, index) => {
     var sub_string = `SDR ${key}:`;
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${decoders[key].Status == "Ok" ? "green" : "red"}>${decoders[key].Status}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(55, ".")}<strong><span class=${
+      decoders[key].Status == "Ok" ? "green" : "red"
+    }>${decoders[key].Status}</span></strong>`;
+    html_output += "<br>";
   });
 
   keys_servers.forEach((key, index) => {
     var sub_string = `Internal Server ${key}:`;
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${servers[key].Status == "Ok" ? "green" : "red"}>${servers[key].Status}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(55, ".")}<strong><span class=${
+      servers[key].Status == "Ok" ? "green" : "red"
+    }>${servers[key].Status}</span></strong>`;
+    html_output += "<br>";
     sub_string = `Internal Server ${key} to Python Connecton:`;
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${servers[key].Web == "Ok" ? "green" : "red"}>${servers[key].Web}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(55, ".")}<strong><span class=${
+      servers[key].Web == "Ok" ? "green" : "red"
+    }>${servers[key].Web}</span></strong>`;
+    html_output += "<br>";
   });
 
   keys_stats.forEach((key, index) => {
     var sub_string = `Internal Stat Server ${key}:`;
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${stats[key].Status == "Ok" ? "green" : "red"}>${stats[key].Status}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(55, ".")}<strong><span class=${
+      stats[key].Status == "Ok" ? "green" : "red"
+    }>${stats[key].Status}</span></strong>`;
+    html_output += "<br>";
   });
 
   keys_feeders.forEach((key, index) => {
     var sub_string = `Airframes.io Feeders ${key}:`;
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${feeders[key].Status == "Ok" ? "green" : "red"}>${feeders[key].Status}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(55, ".")}<strong><span class=${
+      feeders[key].Status == "Ok" ? "green" : "red"
+    }>${feeders[key].Status}</span></strong>`;
+    html_output += "<br>";
   });
 
   keys_receivers.forEach((key, index) => {
     var sub_string = `${key} Receiving Messages:`;
     var class_string = "";
-    if(receivers[key].Status == "Ok")
-      class_string = '"green"';
-    else
-      class_string = receivers[key].Status == "Bad" ? "red" : "orange";
+    if (receivers[key].Status == "Ok") class_string = '"green"';
+    else class_string = receivers[key].Status == "Bad" ? "red" : "orange";
 
-    html_output += `${sub_string.padEnd(55,'.')}<strong><span class=${class_string}>${receivers[key].Status}</span></strong>`;
-    html_output += '<br>';
+    html_output += `${sub_string.padEnd(
+      55,
+      "."
+    )}<strong><span class=${class_string}>${
+      receivers[key].Status
+    }</span></strong>`;
+    html_output += "<br>";
   });
 
-  html_output += '</span>';
+  html_output += "</span>";
 
   return html_output;
 }
