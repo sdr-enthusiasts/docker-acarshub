5be56647f2745e06a798e0bffa51da8563a7c3c7
Fred <43556888+fredclausen@users.noreply.github.com>
2021-05-25 14:43:21 -0600

    typescript docker build + github test build

--- acarshub-typescript/build.sh
+++ acarshub-typescript/build.sh
@@ -2,12 +2,11 @@
 
 # ./node_modules/.bin/tsc
 
-pushd ./dist
-find . -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import Cookies from \"js-cookie\";/import Cookies from \".\/other\/js.cookie.min.mjs\";/g;" {} \;
-find . -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import { MessageDecoder } from \"@airframes\/acars-decoder\/dist\/MessageDecoder\";/import { MessageDecoder } from \"..\/airframes-acars-decoder\/MessageDecoder.js\"/g;" {} \;
+find ./dist/ -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import Cookies from \"js-cookie\";/import Cookies from \".\/other\/js.cookie.min.mjs\";/g;" {} \;
+find ./dist/ -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import { MessageDecoder } from \"@airframes\/acars-decoder\/dist\/MessageDecoder\";/import { MessageDecoder } from \"..\/airframes-acars-decoder\/MessageDecoder.js\"/g;" {} \;
 
-find . -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import { Chart } from \"chart.js\";//g" {} \;
-find . -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import ChartDataLabels from \"chartjs-plugin-datalabels\";//g" {} \;
-find . -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import showdown from \"showdown\";//g" {} \;
+find ./dist/ -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import { Chart } from \"chart.js\";//g" {} \;
+find ./dist/ -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import ChartDataLabels from \"chartjs-plugin-datalabels\";//g" {} \;
+find ./dist/ -maxdepth 1 -type f -iname '*.js' -exec sed -i.bu "s/import showdown from \"showdown\";//g" {} \;
 
-rm *.bu
+rm -f ./dist/*.bu
--- build-test-no-cluster.sh
+++ build-test-no-cluster.sh
@@ -20,8 +20,16 @@ mv rootfs/webapp/static/airframes-acars-decoder .
 # Build airframesio/acars-decoder-typescript
 # Copy /src/acars-decoder-typescript.tgz out of image
 docker build --file ./Dockerfile.acars-decoder-typescript -t acars-decoder-typescript:latest .
+
 id=$(docker create acars-decoder-typescript:latest)
 docker cp "$id":/src/acars-decoder-typescript.tgz ./acars-decoder-typescript.tgz
+docker rm -v "$id"
+
+# build the acarshub typescript
+docker build --file ./Dockerfile.acarshub-typescript -t acarshub-typescript:latest .
+
+id=$(docker create acarshub-typescript:latest)
+docker cp "$id":/rootfs/webapp/static/js .
 docker rm -v "$id"
 
 # Build & push latest
@@ -29,6 +37,7 @@ docker build -f Dockerfile.local -t "${REPO}/${IMAGE}:test" .
 
 # Clean up
 rm ./acars-decoder-typescript.tgz
+rm -rf js
 
 #  move the local built copy of airframes out of rootfs
 mv airframes-acars-decoder rootfs/webapp/static/airframes-acars-decoder
