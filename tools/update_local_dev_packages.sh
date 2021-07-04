5cb2cd3e5aa34f83585bf18214566171b8e66908
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2021-07-04 17:06:21 -0600

    Linting shell check

--- tools/update_local_dev_packages.sh
+++ tools/update_local_dev_packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pushd ../rootfs/webapp
+pushd ../rootfs/webapp || exit 1
 
 < requirements.txt grep -v 'rrdtool' | xargs -I {} python3 -m pip install {}
 
-popd
-pushd ../acarshub-typescript
+popd || exit 1
+pushd ../acarshub-typescript || exit 1
 npm i
