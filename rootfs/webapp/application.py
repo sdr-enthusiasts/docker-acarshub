e2766e4053a8c654c48812f7c760f58dc150ca43
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2021-07-11 08:59:12 -0600

    Run pruning every 15 minutes

--- rootfs/webapp/application.py
+++ rootfs/webapp/application.py
@@ -128,11 +128,14 @@ def scheduled_tasks():
     # init the dbs if not already there
 
     if not acarshub_helpers.SPAM:
+        schedule.every().minute.at(":15").do(acarshub.service_check)
         schedule.every().minute.at(":00").do(update_rrd_db)
         schedule.every().minute.at(":30").do(acarshub_rrd.update_graphs)
-        schedule.every().minute.at(":15").do(acarshub.service_check)
-        # Run and Schedule the database pruner
+    # Run and Schedule the database pruner
+    schedule.every().hour.at(":00").do(acarshub.acarshub_db.pruneOld)
+    schedule.every().hour.at(":15").do(acarshub.acarshub_db.pruneOld)
     schedule.every().hour.at(":30").do(acarshub.acarshub_db.pruneOld)
+    schedule.every().hour.at(":45").do(acarshub.acarshub_db.pruneOld)
 
     # Check for dead threads and restart
     schedule.every().minute.at(":45").do(
