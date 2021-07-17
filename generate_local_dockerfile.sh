99a161cc144166e6c0f37d53de2b0b6451619c5e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2021-07-17 11:20:56 -0600

    local build

--- generate_local_dockerfile.sh
+++ generate_local_dockerfile.sh
@@ -18,5 +18,13 @@ sed -i.bu '/tar -xzvf \/src\/webapp.tar.gz -C \/ && \\/d' Dockerfile.local
 # move the COPY FS line back to the bottom so that we can use cached builds
 sed -i.bu 's/ENTRYPOINT \[ "\/init" \]/COPY rootfs\/ \/\nCOPY webapp\/ \/webapp\/\nENTRYPOINT \[ "\/init" \]\n/g' Dockerfile.local
 
+# fix the import for Message Decoder
+sed -i.bu '/    export INDEX_PATH=\$(ls \/webapp\/static\/js\/index\*.js) && \\/d' Dockerfile.local
+sed -i.bu '/    export OLD_MD5=\$(md5sum \$INDEX_PATH | awk -F\x27 \x27 \x27{print \$1\}\x27) && \\/d' Dockerfile.local
+sed -i.bu '/    echo \$(basename \/webapp\/static\/airframes-acars-decoder\/MessageDecoder\*.js) | xargs -I \x27{}\x27 sed -i "s\/MessageDecoder.js\/\$(echo {})\/g" \$INDEX_PATH && \\/d' Dockerfile.local
+sed -i.bu '/    export NEW_MD5=\$(md5sum \$INDEX_PATH | awk -F\x27 \x27 \x27{print \$1}\x27) && \\/d' Dockerfile.local
+sed -i.bu '/    mv \$INDEX_PATH \$(echo \$INDEX_PATH | sed -e "s\/\$OLD_MD5\/\$NEW_MD5\/g") && \\/d' Dockerfile.local
+sed -i.bu '/    sed -i "s\/\$(echo \$OLD_MD5)\/\$(echo \$NEW_MD5)\/g" \/webapp\/templates\/index.html && \\/d' Dockerfile.local
+
 # clean up...thanks mac
 rm -f Dockerfile.local.bu
