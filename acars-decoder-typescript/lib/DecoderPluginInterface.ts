b09291310872b3b4466028aced2ffa09f888471c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2021-08-07 14:11:14 -0600

    remove old version of subtree

--- acars-decoder-typescript/lib/DecoderPluginInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface DecoderPluginInterface {
-  decode(message: any) : any;
-  meetsStateRequirements() : boolean;
-  // onRegister(store: Store<any>) : void;
-  qualifiers() : any;
-}
-
-export default {
-}
