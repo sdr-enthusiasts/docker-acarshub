14b06019bae1d10e04d2aef0f31e20dff2498e57
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-01-08 12:15:54 -0700

    Update typescript installer scripts

--- acarshub-typescript/copy_test_assets.sh
+++ acarshub-typescript/copy_test_assets.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm -f ../rootfs/webapp/images/*hour.png
-rm -f ../rootfs/webapp/images/*hours.png
-rm -f ../rootfs/webapp/images/*days.png
-rm -f ../rootfs/webapp/images/*week.png
-rm -f ../rootfs/webapp/images/*months.png
-rm -f ../rootfs/webapp/images/*year.png
+rm -f ../rootfs/webapp/static/images/*hour.png
+rm -f ../rootfs/webapp/static/images/*hours.png
+rm -f ../rootfs/webapp/static/images/*days.png
+rm -f ../rootfs/webapp/static/images/*week.png
+rm -f ../rootfs/webapp/static/images/*months.png
+rm -f ../rootfs/webapp/static/images/*year.png
 
-cp  test_assets/images/* ../rootfs/webapp/images/
+cp  test_assets/images/* ../rootfs/webapp/static/images/
 
 rm -f ../rootfs/webapp/data/ground-stations.json
 rm -f ../rootfs/webapp/data/acars-metadata.json
--- acarshub-typescript/installer.sh
+++ acarshub-typescript/installer.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-rm -rf ../rootfs/webapp/*
-cp ./html/* ../rootfs/webapp/static/
+# Clean up the install dir
+rm -rf ../rootfs/webapp/static/images/* || exit 1
+rm -rf ../rootfs/webapp/static/js/* || exit 1
+rm -rf ../rootfs/webapp/static/sounds/* || exit 1
+rm -rf ../rootfs/webapp/templates/* || exit 1
+
+# recreate the dir structure
+
+mkdir -p ../rootfs/webapp/static/images || exit 1
+mkdir -p ../rootfs/webapp/static/js || exit 1
+mkdir -p ../rootfs/webapp/static/sounds  || exit 1
+mkdir -p ../rootfs/webapp/templates || exit 1
+
+# copy the assets to the correct place
+cp -r ./dist/static/images ../rootfs/webapp/static/ || exit 1
+cp -r ./dist/static/sounds ../rootfs/webapp/static/ || exit 1
+cp -r ./dist/static/js ../rootfs/webapp/static/ || exit 1
+cp -r ./dist/static/index.html ../rootfs/webapp/templates/ || exit 1
+cp -r ./dist/static/js/helppage.MD ../rootfs/webapp/templates/ || exit 1
