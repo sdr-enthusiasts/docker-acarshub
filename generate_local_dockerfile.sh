9bfc1e0d3757fd35356a54ba0f0f75c33e9695df
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-01-14 17:33:56 -0700

    optimize build step

--- generate_local_dockerfile.sh
+++ generate_local_dockerfile.sh
@@ -12,4 +12,4 @@ sed -i '/COPY rootfs\/ \//d' Dockerfile.local
 sed -i '/ADD webapp.tar.gz \//d' Dockerfile.local
 
 # move the COPY FS line back to the bottom so that we can use cached builds
-sed -i 's/ENTRYPOINT \[ "\/init" \]/COPY rootfs\/ \/\nCOPY webapp\/ \/webapp\/\nCOPY version \/acarshub-version\nENTRYPOINT \[ "\/init" \]\n/g' Dockerfile.local
+sed -i 's/ENTRYPOINT \[ "\/init" \]/COPY version \/acarshub-version\nCOPY rootfs\/ \/\nCOPY webapp\/ \/webapp\/\nENTRYPOINT \[ "\/init" \]\n/g' Dockerfile.local
