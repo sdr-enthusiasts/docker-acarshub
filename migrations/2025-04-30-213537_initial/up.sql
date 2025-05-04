561d76ee506b15243312b1d4cfde1173b3fc3189
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-05-04 01:11:28 -0600

    full acarshub existing db schema in diesel

--- migrations/2025-04-30-213537_initial/up.sql
+++ migrations/2025-04-30-213537_initial/up.sql
@@ -1,9 +1,44 @@
-CREATE TABLE count (id INTEGER NOT NULL,total INTEGER, errors INTEGER, good INTEGER, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS count (id INTEGER NOT NULL,total INTEGER, errors INTEGER, good INTEGER, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS freqs (it INTEGER NOT NULL, freq VARCHAR(32), freq_type VARCHAR(32), count INTEGER, PRIMARY KEY(it));
+CREATE TABLE IF NOT EXISTS level (id INTEGER NOT NULL, level INTEGER, count INTEGER, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS messages (id INTEGER NOT NULL, message_type VARCHAR(32) NOT NULL, msg_time INTEGER NOT NULL,station_id VARCHAR(32) NOT NULL, toaddr VARCHAR(32) NOT NULL, fromaddr VARCHAR(32) NOT NULL, depa VARCHAR(32) NOT NULL, dsta VARCHAR(32) NOT NULL, eta VARCHAR(32) NOT NULL, gtout VARCHAR(32) NOT NULL, gtin VARCHAR(32) NOT NULL, wloff VARCHAR(32) NOT NULL, wlin VARCHAR(32) NOT NULL, lat VARCHAR(32) NOT NULL, lon VARCHAR(32) NOT NULL, alt VARCHAR(32) NOT NULL, msg_text TEXT NOT NULL, tail VARCHAR(32) NOT NULL, flight VARCHAR(32) NOT NULL, icao VARCHAR(32) NOT NULL, freq VARCHAR(32) NOT NULL, ack VARCHAR(32) NOT NULL, mode VARCHAR(32) NOT NULL, label VARCHAR(32) NOT NULL, block_id VARCHAR(32) NOT NULL, msgno VARCHAR(32) NOT NULL, is_response VARCHAR(32) NOT NULL, is_onground VARCHAR(32) NOT NULL, error VARCHAR(32) NOT NULL, libacars TEXT NOT NULL,level VARCHAR(32) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS messages_saved (id INTEGER NOT NULL, message_type VARCHAR(32) NOT NULL, msg_time INTEGER NOT NULL, station_id VARCHAR(32) NOT NULL, toaddr VARCHAR(32) NOT NULL, fromaddr VARCHAR(32) NOT NULL, depa VARCHAR(32) NOT NULL, dsta VARCHAR(32) NOT NULL, eta VARCHAR(32) NOT NULL, gtout VARCHAR(32) NOT NULL, gtin VARCHAR(32) NOT NULL, wloff VARCHAR(32) NOT NULL, wlin VARCHAR(32) NOT NULL, lat VARCHAR(32) NOT NULL, lon VARCHAR(32) NOT NULL, alt VARCHAR(32) NOT NULL, msg_text TEXT NOT NULL,tail VARCHAR(32) NOT NULL, flight VARCHAR(32) NOT NULL, icao VARCHAR(32) NOT NULL, freq VARCHAR(32) NOT NULL, ack VARCHAR(32) NOT NULL, mode VARCHAR(32) NOT NULL, label VARCHAR(32) NOT NULL, block_id VARCHAR(32) NOT NULL, msgno VARCHAR(32) NOT NULL, is_response VARCHAR(32) NOT NULL, is_onground VARCHAR(32) NOT NULL, error VARCHAR(32) NOT NULL, libacars TEXT NOT NULL, level VARCHAR(32) NOT NULL, term VARCHAR(32) NOT NULL, type_of_match VARCHAR(32) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS alert_stats ( id INTEGER NOT NULL, term VARCHAR(32), count INTEGER, PRIMARY KEY (id) );
+CREATE TABLE IF NOT EXISTS ignore_alert_terms ( id INTEGER NOT NULL, term VARCHAR(32), PRIMARY KEY (id) );
+CREATE TABLE IF NOT EXISTS nonlogged_count ( id INTEGER NOT NULL, errors INTEGER, good INTEGER, PRIMARY KEY (id) );
 
-CREATE TABLE freqs (it INTEGER NOT NULL, freq VARCHAR(32), freq_type VARCHAR(32), count INTEGER, PRIMARY KEY(it));
+CREATE VIRTUAL TABLE IF NOT EXISTS messages_fts USING fts5
+(
+    depa,dsta,msg_text,tail,flight,icao,freq,label,
+    content=messages,
+    content_rowid=id
+);
 
-CREATE TABLE level (id INTEGER NOT NULL, level INTEGER, count INTEGER, PRIMARY KEY(id));
+CREATE TRIGGER IF NOT EXISTS messages_fts_insert AFTER INSERT ON messages
+BEGIN
+    INSERT INTO messages_fts (rowid, depa,dsta,msg_text,tail,flight,icao,freq,label) VALUES (new.id, new.depa,new.dsta,new.msg_text,new.tail,new.flight,new.icao,new.freq,new.label);
+END;
 
-CREATE TABLE messages (id INTEGER NOT NULL, message_type VARCHAR(32) NOT NULL, msg_time INTEGER NOT NULL,station_id VARCHAR(32) NOT NULL, toaddr VARCHAR(32) NOT NULL, fromaddr VARCHAR(32) NOT NULL, depa VARCHAR(32) NOT NULL, dsta VARCHAR(32) NOT NULL, eta VARCHAR(32) NOT NULL, gtout VARCHAR(32) NOT NULL, gtin VARCHAR(32) NOT NULL, wloff VARCHAR(32) NOT NULL, wlin VARCHAR(32) NOT NULL, lat VARCHAR(32) NOT NULL, lon VARCHAR(32) NOT NULL, alt VARCHAR(32) NOT NULL, msg_text TEXT NOT NULL, tail VARCHAR(32) NOT NULL, flight VARCHAR(32) NOT NULL, icao VARCHAR(32) NOT NULL, freq VARCHAR(32) NOT NULL, ack VARCHAR(32) NOT NULL, mode VARCHAR(32) NOT NULL, label VARCHAR(32) NOT NULL, block_id VARCHAR(32) NOT NULL, msgno VARCHAR(32) NOT NULL, is_response VARCHAR(32) NOT NULL, is_onground VARCHAR(32) NOT NULL, error VARCHAR(32) NOT NULL, libacars TEXT NOT NULL,level VARCHAR(32) NOT NULL, PRIMARY KEY(id));
+CREATE TRIGGER IF NOT EXISTS messages_fts_delete AFTER DELETE ON messages
+BEGIN
+    INSERT INTO messages_fts (messages_fts, rowid, depa,dsta,msg_text,tail,flight,icao,freq,label) VALUES ('delete', old.id, old.depa,old.dsta,old.msg_text,old.tail,old.flight,old.icao,old.freq,old.label);
+END;
 
-CREATE TABLE messages_saved (id INTEGER NOT NULL, message_type VARCHAR(32) NOT NULL, msg_time INTEGER NOT NULL, station_id VARCHAR(32) NOT NULL, toaddr VARCHAR(32) NOT NULL, fromaddr VARCHAR(32) NOT NULL, depa VARCHAR(32) NOT NULL, dsta VARCHAR(32) NOT NULL, eta VARCHAR(32) NOT NULL, gtout VARCHAR(32) NOT NULL, gtin VARCHAR(32) NOT NULL, wloff VARCHAR(32) NOT NULL, wlin VARCHAR(32) NOT NULL, lat VARCHAR(32) NOT NULL, lon VARCHAR(32) NOT NULL, alt VARCHAR(32) NOT NULL, msg_text TEXT NOT NULL,tail VARCHAR(32) NOT NULL, flight VARCHAR(32) NOT NULL, icao VARCHAR(32) NOT NULL, freq VARCHAR(32) NOT NULL, ack VARCHAR(32) NOT NULL, mode VARCHAR(32) NOT NULL, label VARCHAR(32) NOT NULL, block_id VARCHAR(32) NOT NULL, msgno VARCHAR(32) NOT NULL, is_response VARCHAR(32) NOT NULL, is_onground VARCHAR(32) NOT NULL, error VARCHAR(32) NOT NULL, libacars TEXT NOT NULL, level VARCHAR(32) NOT NULL, term VARCHAR(32) NOT NULL, type_of_match VARCHAR(32) NOT NULL, PRIMARY KEY(id));
+CREATE TRIGGER IF NOT EXISTS messages_fts_update AFTER UPDATE ON messages
+BEGIN
+    INSERT INTO messages_fts (messages_fts, rowid, depa,dsta,msg_text,tail,flight,icao,freq,label) VALUES ('delete', old.id, old.depa,old.dsta,old.msg_text,old.tail,old.flight,old.icao,old.freq,old.label);
+    INSERT INTO messages_fts (rowid, depa,dsta,msg_text,tail,flight,icao,freq,label) VALUES (new.id, new.depa,new.dsta,new.msg_text,new.tail,new.flight,new.icao,new.freq,new.label);
+END;
+
+INSERT INTO messages_fts(messages_fts) VALUES ("rebuild");
+
+CREATE INDEX IF NOT EXISTS ix_messages_msg_text ON messages ("msg_text" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_icao ON messages ("icao" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_flight ON messages ("flight" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_tail ON messages ("tail" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_depa ON messages ("depa" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_dsta ON messages ("dsta" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_msgno ON messages ("msgno" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_freq ON messages ("freq" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_label ON messages ("label" DESC);
+CREATE INDEX IF NOT EXISTS ix_messages_msgtime ON "messages" ("msg_time" DESC);
--- rust/schema.rs
+++ rust/schema.rs
@@ -1,1 +1,185 @@
 // @generated automatically by Diesel CLI.
+
+diesel::table! {
+    alert_stats (id) {
+        id -> Integer,
+        term -> Nullable<Text>,
+        count -> Nullable<Integer>,
+    }
+}
+
+diesel::table! {
+    count (id) {
+        id -> Integer,
+        total -> Nullable<Integer>,
+        errors -> Nullable<Integer>,
+        good -> Nullable<Integer>,
+    }
+}
+
+diesel::table! {
+    freqs (it) {
+        it -> Integer,
+        freq -> Nullable<Text>,
+        freq_type -> Nullable<Text>,
+        count -> Nullable<Integer>,
+    }
+}
+
+diesel::table! {
+    ignore_alert_terms (id) {
+        id -> Integer,
+        term -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    level (id) {
+        id -> Integer,
+        level -> Nullable<Integer>,
+        count -> Nullable<Integer>,
+    }
+}
+
+diesel::table! {
+    messages (id) {
+        id -> Integer,
+        message_type -> Text,
+        msg_time -> Integer,
+        station_id -> Text,
+        toaddr -> Text,
+        fromaddr -> Text,
+        depa -> Text,
+        dsta -> Text,
+        eta -> Text,
+        gtout -> Text,
+        gtin -> Text,
+        wloff -> Text,
+        wlin -> Text,
+        lat -> Text,
+        lon -> Text,
+        alt -> Text,
+        msg_text -> Text,
+        tail -> Text,
+        flight -> Text,
+        icao -> Text,
+        freq -> Text,
+        ack -> Text,
+        mode -> Text,
+        label -> Text,
+        block_id -> Text,
+        msgno -> Text,
+        is_response -> Text,
+        is_onground -> Text,
+        error -> Text,
+        libacars -> Text,
+        level -> Text,
+    }
+}
+
+diesel::table! {
+    messages_fts (rowid) {
+        rowid -> Integer,
+        depa -> Nullable<Binary>,
+        dsta -> Nullable<Binary>,
+        msg_text -> Nullable<Binary>,
+        tail -> Nullable<Binary>,
+        flight -> Nullable<Binary>,
+        icao -> Nullable<Binary>,
+        freq -> Nullable<Binary>,
+        label -> Nullable<Binary>,
+        messages_fts -> Nullable<Binary>,
+        rank -> Nullable<Binary>,
+    }
+}
+
+diesel::table! {
+    messages_fts_config (k) {
+        k -> Binary,
+        v -> Nullable<Binary>,
+    }
+}
+
+diesel::table! {
+    messages_fts_data (id) {
+        id -> Nullable<Integer>,
+        block -> Nullable<Binary>,
+    }
+}
+
+diesel::table! {
+    messages_fts_docsize (id) {
+        id -> Nullable<Integer>,
+        sz -> Nullable<Binary>,
+    }
+}
+
+diesel::table! {
+    messages_fts_idx (segid, term) {
+        segid -> Binary,
+        term -> Binary,
+        pgno -> Nullable<Binary>,
+    }
+}
+
+diesel::table! {
+    messages_saved (id) {
+        id -> Integer,
+        message_type -> Text,
+        msg_time -> Integer,
+        station_id -> Text,
+        toaddr -> Text,
+        fromaddr -> Text,
+        depa -> Text,
+        dsta -> Text,
+        eta -> Text,
+        gtout -> Text,
+        gtin -> Text,
+        wloff -> Text,
+        wlin -> Text,
+        lat -> Text,
+        lon -> Text,
+        alt -> Text,
+        msg_text -> Text,
+        tail -> Text,
+        flight -> Text,
+        icao -> Text,
+        freq -> Text,
+        ack -> Text,
+        mode -> Text,
+        label -> Text,
+        block_id -> Text,
+        msgno -> Text,
+        is_response -> Text,
+        is_onground -> Text,
+        error -> Text,
+        libacars -> Text,
+        level -> Text,
+        term -> Text,
+        type_of_match -> Text,
+    }
+}
+
+diesel::table! {
+    nonlogged_count (id) {
+        id -> Integer,
+        errors -> Nullable<Integer>,
+        good -> Nullable<Integer>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    alert_stats,
+    count,
+    freqs,
+    ignore_alert_terms,
+    level,
+    messages,
+    messages_fts,
+    messages_fts_config,
+    messages_fts_data,
+    messages_fts_docsize,
+    messages_fts_idx,
+    messages_saved,
+    nonlogged_count,
+);
