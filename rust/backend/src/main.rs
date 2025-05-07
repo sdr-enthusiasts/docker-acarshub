e5aa849246830f1f5c503ddab0b1a9c04a67f5ca
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-05-07 17:46:39 -0600

    (not final) handle the db url correctly if users are legacy

--- rust/backend/src/main.rs
+++ rust/backend/src/main.rs
@@ -6,7 +6,19 @@ use std::env;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../../migrations");
 fn establish_connection() -> SqliteConnection {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let mut database_url = match env::var("DATABASE_URL") {
+        Ok(url) => url,
+        Err(_) => "/opt/acarshub/messages.sqlite".to_string(),
+    };
+
+    // we need to see if we're on an old version of ACARS Hub. If /run/acarshub/messages.db exists, we need to use that and
+    // inform the user that they need to migrate their database
+
+    if std::path::Path::new("/run/acarshub/messages.db").exists() {
+        database_url = "/run/acarshub/messages.db".to_string();
+        println!("Using old database at /run/acarshub/messages.db. Please migrate your database.");
+    }
+
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
