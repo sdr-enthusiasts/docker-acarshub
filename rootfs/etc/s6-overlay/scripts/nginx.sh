4e41524286ba1894c77b2f0aaf9ddef08aa875c9
Matthias Wirth <matthias.wirth@gmail.com>
2026-03-17 00:13:47 +0000

    log timestamps: use s6wrap instead of piping to awk

--- rootfs/etc/s6-overlay/scripts/nginx.sh
+++ rootfs/etc/s6-overlay/scripts/nginx.sh
@@ -3,11 +3,11 @@
 
 if [[ $((MIN_LOG_LEVEL)) -ge 4 ]]; then
     # shellcheck disable=SC2016
-    echo "Starting web proxy service" | stdbuf -oL awk '{print "[nginx       ] " strftime("%Y/%m/%d %H:%M:%S", systime()) " " $0}'
+    s6wrap --quiet --prepend=nginx --timestamps --args echo "Starting web proxy service"
 fi
 
 # shellcheck disable=SC2016
 mkdir -p /var/log/nginx
 # shellcheck disable=SC2016
 exec \
-    /usr/sbin/nginx -c /etc/nginx.acarshub/nginx.conf 2>&1 | stdbuf -oL awk '{print "[nginx       ] " strftime("%Y/%m/%d %H:%M:%S", systime()) " " $0}'
+    s6wrap --quiet --prepend=nginx --timestamps --args /usr/sbin/nginx -c /etc/nginx.acarshub/nginx.conf
